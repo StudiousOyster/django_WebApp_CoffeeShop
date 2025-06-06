1bb2e310a6ff90fc48375abc75723972302f6a41
Chippy-Mathew <chippym@gmail.com>
2025-06-06 22:32:01 +0530

    new

--- coffee/views.py
+++ coffee/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import coffee_db
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Homepage of the Coffee Shop</h1>')
+    coffee = coffee_db.objects.all()  # Fetch all coffee items from the database
+    return render(request, 'home.html', {'coffee': coffee})
