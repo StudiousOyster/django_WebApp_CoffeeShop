257f03db313b7ebd85520c903d849a5f8c860997
Chippy-Mathew <chippym@gmail.com>
2025-06-06 13:34:13 +0530

    changes coffeeshop

--- coffee/admin.py
+++ coffee/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import coffee_db
 
 # Register your models here.
+class coffee_dbAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity')
+    search_fields = ('name',)
+    list_filter = ('price',)
+
+admin.site.register(coffee_db, coffee_dbAdmin)
